3e224cb62fe2dbad15dfa7d015de2ad27a52bec9
‘Thashmila <thashmilajayasinghe@gmail.com>
2024-11-08 06:36:22 +0530

    Update cors policy in backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import cors from "cors";
 
 import recipeRoutes from "./routes/recipe.routes.js";
 
@@ -10,6 +11,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // middleware that allows the acceptance of JSON in request body
+
+// app.use(cors());
+
+app.use(cors({
+  origin: 'http://localhost:5173/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+}));
 
 app.use("/api/recipes", recipeRoutes);
 
