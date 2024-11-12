09327e42adbeb045d48e9f04138f4674a2906bfd
‘Thashmila <thashmilajayasinghe@gmail.com>
2024-11-12 08:26:34 +0530

    authentication middleware and route protection added

--- backend/routes/recipe.routes.js
+++ backend/routes/recipe.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
-
 import { createRecipe, getAllRecipes, getRecipe, updateRecipe, deleteRecipe } from "../controllers/recipe.controller.js";
+import { authenticate } from "../middleware/authMiddleware.js";
+
 
 const router = express.Router();
 
-router.post("/", createRecipe);
-router.get("/", getAllRecipes);
-router.get("/:id", getRecipe);
-router.put("/:id", updateRecipe);
-router.delete("/:id", deleteRecipe);
+router.post("/", authenticate, createRecipe);
+router.get("/", authenticate, getAllRecipes);
+router.get("/:id", authenticate, getRecipe);
+router.put("/:id", authenticate, updateRecipe);
+router.delete("/:id", authenticate, deleteRecipe);
 
 export default router;
