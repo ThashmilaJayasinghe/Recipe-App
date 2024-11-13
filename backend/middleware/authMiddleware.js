dfb5f01c4fd4f81f40a516dbd2b8fcb7ea4d1232
‘Thashmila <thashmilajayasinghe@gmail.com>
2024-11-13 14:33:04 +0530

    Auth middleware update

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,14 +1,27 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.model.js"
+
+// export const authenticate = (req, res, next) => {
+//   const token = req.header('Authorization').replace('Bearer ', '');
+//   if (!token) return res.status(401).json({ error: 'Access denied' });
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded;
+//     next();
+//   } catch (err) {
+//     res.status(400).json({ error: 'Invalid token' });
+//   }
+// };
+
 
 export const authenticate = (req, res, next) => {
-  const token = req.header('Authorization').replace('Bearer ', '');
-  if (!token) return res.status(401).json({ error: 'Access denied' });
+  const token = req.header('Authorization')?.split(' ')[1];
+  if (!token) return res.status(401).json({ message: 'Unauthorized' });
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: 'Forbidden' });
+    req.user = user;
     next();
-  } catch (err) {
-    res.status(400).json({ error: 'Invalid token' });
-  }
+  });
 };
