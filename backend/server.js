1aa15be5b49c897f0ddf8f50b23ba3b6e4dc06ee
‘Thashmila <thashmilajayasinghe@gmail.com>
2024-11-18 20:24:51 +0530

    files updated for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import cors from "cors";
 
@@ -11,18 +12,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // middleware that allows the acceptance of JSON in request body
 
 app.use(cors());
 
-// app.use(cors({
-//   origin: 'http://localhost:5173/',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type'],
-// }));
-
 app.use("/api/recipes", recipeRoutes);
 app.use("/api/auth", userRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  })
+}
 
 app.listen(PORT, () => {
   connectDB(); 
